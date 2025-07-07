ee8dbf113bf14b431fe7fcbb96581cab9faa06ff
sakshishr <sakshisharma22092006@gmail.com>
2025-07-07 12:14:28 +0530

    added

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ function toggleMenu(){
   const nav=document.querySelector("#navbar")
   nav.classList.toggle("show");
 }
+//Welcome to my portfolio! I'm a web developer passionate about crafting clean,responsive and user-friendly websites.
+const msg ="Welcome to my portfolio! I'm a web developer passionate about crafting clean,responsive and user-friendly websites.";
+const desc =document.getElementById("desc");
+let i=0;
+function typeWriter(){
+  if(i<msg.length){
+    desc.innerHTML+= msg.charAt(i);
+    i++;
+    setTimeout(typeWriter,50);
+  }
+}
+typeWriter();
