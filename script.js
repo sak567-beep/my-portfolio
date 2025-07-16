76ca13e121e6d17583979dd9a26ac1bc90a1bd6e
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-07-16 22:26:47 +0530

    updating nav bar

--- script.js
+++ script.js
@@ -14,3 +14,44 @@ function typeWriter(){
   }
 }
 typeWriter();
+
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("#navbar a");
+
+window.addEventListener("scroll", () => {
+  let scrollY = window.scrollY;
+  let windowHeight = window.innerHeight;
+  let docHeight = document.documentElement.scrollHeight;
+  
+  sections.forEach(section => {
+    const top = section.offsetTop - 120;
+    const bottom = top + section.offsetHeight;
+    const id = section.getAttribute("id");
+
+    // Special check for bottom of page
+    if (scrollY + windowHeight >= docHeight) {
+      // Last section ko active karo
+      navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === `#contact`) {
+          link.classList.add("active");
+        }
+      });
+    } else if (scrollY >= top && scrollY < bottom) {
+      navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === `#${id}`) {
+          link.classList.add("active");
+        }
+      });
+    }
+  });
+});
+
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.forEach(l => l.classList.remove("active"));
+    link.classList.add("active");
+  });
+});
